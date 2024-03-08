abeba5c279cb864d4a8aeca049cf01bf4c5fcef7
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-03-09 00:50:54 +0530

    Trending Products, Best Sales, New Arrivals and Popular Category section on home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{useState, useEffect} from 'react'
 import Helmet from '../components/Helmet/Helmet'
 import {Container, Row, Col} from 'reactstrap'
 import HeroImg from '../assets/images/hero-img.png'
@@ -6,10 +6,40 @@ import '../styles/home.css'
 import { Link } from 'react-router-dom'
 import {motion} from 'framer-motion'
 import Services from '../services/Services'
+import ProductsList from '../components/UI/ProductsList'
+import products from '../assets/data/products'
+import counterImg from '../assets/images/counter-timer-img.png'
+import Clock from '../components/UI/Clock'
 
 const Home = () => {
 
+  const [trendingProducts, setTrendingProducts] = useState([])
+  const [bestSalesProducts, setBestSalesProducts] = useState([])
+  const [mobileProducts, setMobileProducts] = useState([])
+  const [wirelessProducts, setWirelessProducts] = useState([])
+  const [popularProducts, setPopularProducts] = useState([])
+
   const year = new Date().getFullYear()
+
+  useEffect(() => {
+    const filteredTrendingProducts = products.filter(item => item.category === "chair")
+
+    const filteredBestSalesProducts = products.filter(item => item.category === "sofa")
+
+    const filteredMobileProducts = products.filter(item => item.category === "mobile")
+
+    const filteredWirelessProducts = products.filter(item => item.category === "wireless")
+
+    const filteredPopularProducts = products.filter(item => item.category === "watch")
+
+    setTrendingProducts(filteredTrendingProducts)
+    setBestSalesProducts(filteredBestSalesProducts)
+    setMobileProducts(filteredMobileProducts)
+    setWirelessProducts(filteredWirelessProducts)
+    setPopularProducts(filteredPopularProducts)
+
+  }, [])
+
 
   return <Helmet title={"Home"}>
     <section className="hero_section">
@@ -35,8 +65,71 @@ const Home = () => {
     </section>
 
     <Services />
+    <section className="trending_products">
+      <Container>
+        <Row>
+          <Col lg='12' className='text-center'>
+            <h2 className="section_title">Trending Products</h2>
+          </Col>
+          <ProductsList data={trendingProducts}/>
+        </Row>
+      </Container>
+    </section>
+
+    <section className="best_sales">
+      <Container>
+      <Row>
+          <Col lg='12' className='text-center'>
+            <h2 className="section_title">Best Sales</h2>
+          </Col>
+          <ProductsList data={bestSalesProducts}/>
+        </Row>
+      </Container>
+    </section>
+
+    <section className="timer_count">
+      <Container>
+        <Row>
+          <Col lg='6' md='6'>
+            <div className="clock_top-content">
+              <h4 className='text-white fs-6 mb-2'>Limited Offers</h4>
+              <h3 className='text-white fs-5 mb-3'>Quality Armchair</h3>
+            </div>
+            <Clock />
+            <motion.button whileTap={{scale:1.2}} className="buy_btn store_btn"><Link to='/shop'>Visit Store</Link></motion.button>
+          </Col>
+          <Col lg='6' md='6' className='text-end'>
+            <img src={counterImg} alt="" />
+          </Col>
+
+        </Row>
+      </Container>
+    </section>
+
+    <section className="new_arrivals">
+      <Container>
+        <Row>
+          <Col lg='12' className='text-center mb-5'>
+            <h2 className="section_title">New Arrivals</h2>
+          </Col>
+          <ProductsList data={mobileProducts}/>
+          <ProductsList data={wirelessProducts}/>
+        </Row>
+      </Container>
+    </section>
+
+    <section className="popular_category">
+      <Container>
+        <Row>
+          <Col lg='12' className='text-center mb-5'>
+            <h2 className="section_title">Popular in Category</h2>
+          </Col>
+          <ProductsList data={popularProducts}/>
+        </Row>
+      </Container>
+    </section>
+
     </Helmet>;
-  
 }
 
 export default Home
