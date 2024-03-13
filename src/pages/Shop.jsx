4fa3c61058dac2a7080471eab668cc974b8123ea
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-03-13 23:43:38 +0530

    Added ProductDetails Page along with responsiveness

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -51,7 +51,7 @@ const Shop = () => {
       <section>
         <Container>
           <Row>
-            <Col lg='3' md='3'>
+            <Col lg='3' md='6'>
               <div className="filter_widget">
                 <select onChange={handleFilter}>
                   <option>Filter By Category</option>
@@ -63,7 +63,7 @@ const Shop = () => {
                 </select>
               </div>
             </Col>
-            <Col lg='3' md='3'>
+            <Col lg='3' md='6' className='text-end'>
             <div className="filter_widget">
                 <select>
                   <option>Sort By</option>
@@ -72,7 +72,7 @@ const Shop = () => {
                 </select>
               </div>
             </Col>
-            <Col lg='6' md='6'>
+            <Col lg='6' md='12'>
               <div className="search_box">
                 <input type="text" placeholder='Search....' onChange={handleSearch}/>
                 <span><i class="ri-search-line"></i></span>
