5c176b50a10bf8fe7ddded6dfa0679d83c40e616
Abk0710 <muruganabhishekinft-@atharvacoe.ac.in>
2024-10-21 23:37:28 +0530

    Trending and best sold products updated successfully

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,36 +10,30 @@ import ProductsList from '../components/UI/ProductsList'
 import counterImg from '../assets/images/counter-timer-img.png'
 import Clock from '../components/UI/Clock'
 import useGetData from '../custom-hooks/useGetData'
+import products from '../assets/data/products'
 
 const Home = () => {
-
-  const {data:products, loading} = useGetData('products')
+  const year = new Date().getFullYear();
   const [trendingProducts, setTrendingProducts] = useState([])
   const [bestSalesProducts, setBestSalesProducts] = useState([])
   const [mobileProducts, setMobileProducts] = useState([])
   const [wirelessProducts, setWirelessProducts] = useState([])
   const [popularProducts, setPopularProducts] = useState([])
 
-  const year = new Date().getFullYear()
+  
 
   useEffect(() => {
     const filteredTrendingProducts = products.filter(item => item.category === "chair")
-
     const filteredBestSalesProducts = products.filter(item => item.category === "sofa")
-
     const filteredMobileProducts = products.filter(item => item.category === "mobile")
-
     const filteredWirelessProducts = products.filter(item => item.category === "wireless")
-
     const filteredPopularProducts = products.filter(item => item.category === "watch")
-
     setTrendingProducts(filteredTrendingProducts)
     setBestSalesProducts(filteredBestSalesProducts)
     setMobileProducts(filteredMobileProducts)
     setWirelessProducts(filteredWirelessProducts)
     setPopularProducts(filteredPopularProducts)
-
-  }, [products])
+  }, [])
 
 
   return <Helmet title={"Home"}>
@@ -72,32 +66,24 @@ const Home = () => {
           <Col lg='12' className='text-center'>
             <h2 className="section_title">Trending Products</h2>
           </Col>
-          {
-            loading ? <h5 className='fw-bold'>Loading...</h5> : 
-            <ProductsList data={trendingProducts}/>
-          }
+          <ProductsList data={trendingProducts}/>
         </Row>
       </Container>
     </section>
-
     <section className="best_sales">
       <Container>
       <Row>
           <Col lg='12' className='text-center'>
             <h2 className="section_title">Best Sales</h2>
           </Col>
-          {
-            loading ? <h5 className='fw-bold'>Loading...</h5> : 
-            <ProductsList data={bestSalesProducts}/>
-          }
+          <ProductsList data={bestSalesProducts}/>
         </Row>
       </Container>
     </section>
-
     <section className="timer_count">
       <Container>
         <Row>
-          <Col lg='6' md='12' className='count_down-col'>
+          <Col lg='6' md='6'>
             <div className="clock_top-content">
               <h4 className='text-white fs-6 mb-2'>Limited Offers</h4>
               <h3 className='text-white fs-5 mb-3'>Quality Armchair</h3>
@@ -105,42 +91,30 @@ const Home = () => {
             <Clock />
             <motion.button whileTap={{scale:1.2}} className="buy_btn store_btn"><Link to='/shop'>Visit Store</Link></motion.button>
           </Col>
-          <Col lg='6' md='12' className='text-end counter_img'>
+          <Col lg='6' md='6' className='text-end'>
             <img src={counterImg} alt="" />
           </Col>
-
         </Row>
       </Container>
     </section>
-
     <section className="new_arrivals">
       <Container>
         <Row>
           <Col lg='12' className='text-center mb-5'>
             <h2 className="section_title">New Arrivals</h2>
           </Col>
-          {
-            loading ? <h5 className='fw-bold'>Loading...</h5> : 
-            <ProductsList data={mobileProducts}/>
-          }
-          {
-            loading ? <h5 className='fw-bold'>Loading...</h5> : 
-            <ProductsList data={wirelessProducts}/>           
-          }
+          <ProductsList data={mobileProducts}/>
+          <ProductsList data={wirelessProducts}/>
         </Row>
       </Container>
     </section>
-
     <section className="popular_category">
       <Container>
         <Row>
           <Col lg='12' className='text-center mb-5'>
             <h2 className="section_title">Popular in Category</h2>
           </Col>
-          {
-            loading ? <h5 className='fw-bold'>Loading...</h5> : 
-            <ProductsList data={popularProducts}/>           
-          }
+          <ProductsList data={popularProducts}/>
         </Row>
       </Container>
     </section>
